befdc5704e312c7c4d9c8360d91e7ceceeb5c164
HermouetLucas <lucas.hermouet22@gmail.com>
2025-04-22 17:55:06 +0200

    débutPlante

---
+++ ENSCemance/Plante.cs
@@ -0,0 +1,25 @@
+public abstract class Plante{
+    protected  string nom;
+    protected string espece
+    protected bool comestible; 
+    protected  bool mauvaiseHerbe;
+    protected string saisonSemis;
+    protected string terrainPref;
+    protected int espacement;
+    protected float vitesseCroissance;
+    protected float besoinEau; 
+    protected float besoinLumi;
+    protected float besoinNutritif;
+    protected float besoinTemp;
+    protected float zoneTemp;
+    protected List<(Maladie, double)>  maladies;
+    protected float esperenceVie;
+    protected float nbPousse;
+    protected bool estVivante;
+    protected int CompteurDecomposition;
+    protected bool estEnvahissante;
+    protected int etapeDeVie;
+    protected List<(x,y,taille)> OrientationTaillepousse;  //Faire x*t puis y*t
+    protected bool estMalade;
+    protected List<Maladie> maladie;
+}
