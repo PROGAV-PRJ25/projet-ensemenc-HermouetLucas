440a2ba5bc69c5e932af4e518cfa147e905ced02
Alban CAMPIONI <acampioni@ensc.fr>
2025-05-05 15:03:09 +0200

    Terrain

---
+++ ENSCemance/Terrain.cs
@@ -0,0 +1,15 @@
+public class Terrain{
+    private float temperature;
+    private float humidite;
+    private float luminosite;
+    private float nutrition;
+    private List<Intru> intruPossible;
+    private List<Maladie> maladiePossible;
+    private string typeTerrain;
+    private float indiceTemp;
+    private float indiceHum;
+    private float indiceLum;
+    private float indiceNutrition;
+    private int taillePotager;
+    private List<List<Plante>> potager;
+}
