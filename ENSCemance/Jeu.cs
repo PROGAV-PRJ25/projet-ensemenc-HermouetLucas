7631eb8544b20622f02dc702427d30a5ca88f1bb
Alban CAMPIONI <acampioni@ensc.fr>
2025-05-05 15:09:17 +0200

    Jeu

---
+++ ENSCemance/Jeu.cs
@@ -0,0 +1,21 @@
+public class Jeu{
+    private int semaine;
+    private int mois;
+    private int annee;
+    private string saison;
+    private List<List<Terrain>> jardin;
+
+    public void UpdateNouvelleSaison(){}
+    public void UpdateTemps(){}
+    public void AugmenterNutrition(){}
+    public void Deserber(){}
+    private void Pailler(){}
+    private void Arroser(){}
+    private void Traiter(){}
+    private void Semer(){}
+    private void Recolter(){}
+    private void InstallerSerre(){}
+    private void MettreBarriere(){}
+    private void MettrePareSoleil(){}
+    public void FaireAction(){}
+}
