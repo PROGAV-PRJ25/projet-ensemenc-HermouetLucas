89a5de59579d6f841aa1e95f874426fa3265b71f
{Maxence= <Maxence.DE_MONGELAS@etu.uca.fr>
2025-05-23 21:47:29 +0200

    Actualisation de version

--- ENSCemance/Terrain.cs
+++ ENSCemance/Terrain.cs
@@ -31,72 +31,6 @@ public abstract class Terrain
 
     #region Propriétés
 
-<<<<<<< HEAD
-
-    /// Température actuelle du terrain.
-
-    public float Temperature { get => temperature; set => temperature = value; }
-
-
-    /// Humidité actuelle du terrain.
-
-    public float Humidite { get => humidite; set => humidite = value; }
-
-
-    /// Luminosité actuelle du terrain.
-
-    public float Luminosite { get => luminosite; set => luminosite = value; }
-
-
-    /// Nutrition actuelle du terrain.
-
-    public float Nutrition { get => nutrition; set => nutrition = value; }
-
-
-    /// Indice d'influence saisonnière sur la température.
-
-    public float IndiceTemp { get => indiceTemp; set => indiceTemp = value; }
-
-
-    /// Indice d'influence saisonnière sur l'humidité.
-
-    public float IndiceHum { get => indiceHum; set => indiceHum = value; }
-
-
-    /// Indice d'influence saisonnière sur la luminosité.
-
-    public float IndiceLum { get => indiceLum; set => indiceLum = value; }
-
-
-    /// Indice d'influence des plantes sur la nutrition.
-
-    public float IndiceNutrition { get => indiceNutrition; set => indiceNutrition = value; }
-
-
-    /// Type de ce terrain (ex: "Forestier", "Désertique").
-
-    public string TypeTerrain => typeTerrain;
-
-
-    /// Liste des intrus pouvant apparaître sur ce terrain.
-
-    public List<Intru> IntruPossible => intruPossible;
-
-
-    /// Liste des maladies pouvant affecter ce terrain.
-
-    public List<Maladie> MaladiePossible => maladiePossible;
-
-
-    /// Taille initiale du potager (côté du carré).
-
-    public int TaillePotager => taillePotager;
-
-
-    /// Matrice des plantes plantées sur ce terrain.
-
-    public List<List<Plante>> Potager => potager;
-=======
     public float Temperature
     {
         get { return temperature; }
@@ -167,7 +101,6 @@ public abstract class Terrain
     {
         get { return potager.Count; }
     }
->>>>>>> 29ea854a292df918e1ac74b10c2974402623da99
 
     public int DimensionPotager
     {
@@ -182,21 +115,9 @@ public abstract class Terrain
     #endregion
 
 
-<<<<<<< HEAD
-    /// Constructeur protégé de la classe abstraite Terrain.
-
-    protected Terrain(
-        string typeTerrain,
-        float indiceTemp,
-        float indiceHum,
-        float indiceLum,
-        float indiceNutrition,
-        int taillePotager)
-=======
     /// Constructeur protégé initialisant tous les attributs selon le type de terrain.
 
     protected Terrain(string terrainType, int taillePotager)
->>>>>>> 29ea854a292df918e1ac74b10c2974402623da99
     {
         this.terrainType = terrainType;
         this.taillePotager = taillePotager;
@@ -345,13 +266,10 @@ public abstract class Terrain
     }
 
 
-<<<<<<< HEAD
-=======
     /// Plante une nouvelle plante à la position (ligne, colonne) si la case est vide.
 
   
 
->>>>>>> 29ea854a292df918e1ac74b10c2974402623da99
     /// Met à jour les états du terrain en fonction de la saison et des plantes.
 
     public abstract void MettreAJourEtat();
