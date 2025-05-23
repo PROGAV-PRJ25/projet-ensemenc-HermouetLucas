7557c68dc5c93e63cba83e21a6c3849b60206496
AlCampioni <alban.campioni@gmail.com>
2025-05-23 22:41:06 +0200

    changement

--- ENSCemance/Jeu.cs
+++ ENSCemance/Jeu.cs
@@ -12,13 +12,17 @@ public class Jeu
     public int positionCurseurTerrain = 0;
 
 
-    // A mettre dans le terrain plutôt ? 
-    // Calcule la dimension (côté) du jardin (carré)
     private int DimensionJardin
     {
         get { return (int)Math.Sqrt(jardin.Count); }
     }
+    public Jeu()
+    {
+        for (int i = 0; i < 64; i++)
+        {
 
+        }
+    }
 
     public void UpdateNouvelleSaison(float indice)
     {
@@ -132,6 +136,15 @@ public class Jeu
             bool selectionTerminer = true;
             while (selectionTerminer)
             {
+                Console.Clear();
+                Terrain terrainActuel = jardin[positionTerrainSelectionner];
+                // Affichage du potager (utiliser AffichagePotager)
+                var affichage = new AffichagePotager();
+                affichage.potager = terrainActuel.Potager;
+                affichage.taillePotager = terrainActuel.taillePotager;
+                affichage.positionCurseur = positionCurseurTerrain;
+                affichage.AffichageComplet();
+
                 Console.WriteLine();
                 Console.WriteLine("Sélectionner une action à faire : ");
                 Console.WriteLine(" - Désherber : appuyer sur d");
